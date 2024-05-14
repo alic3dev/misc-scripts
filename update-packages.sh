7576a39ba13e58c014df6f10796b3ce847c3c1ae
alic3 <alic3dev@gmail.com>
2024-05-14 04:55:26 -0400

    Move shared variables into seperate script

--- update-packages.sh
+++ update-packages.sh
@@ -1,13 +1,6 @@
 #!/bin/zsh
 
-# Color Variables
-COLOR_RESET="\e[0m"
-COLOR_BLUE="\e[34m"
-COLOR_GREEN="\e[32m"
-COLOR_BOLD="\e[1m"
-
-# Misc Variables
-FIRST_PRINT=true
+. $(dirname "$0")/shared-variables.sh
 
 printUpdatingMessageFor() {
   if $FIRST_PRINT
